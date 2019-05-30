dfba1e66ae03b8e9b0bbdfcf494051e583d8c59b
bianartola <bian.artola@hotmail.com>
2019-05-30 15:07:34 -0300

    Eliminacion de archivos

--- Proyectos/Archivos.H/Registros/answers-service.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-struct RES_RECORD answers[20];
-
-void logicaAnswer(int i, int finalizar, struct DNS_HEADER *dns,
-    struct RES_RECORD answers[20], unsigned char buf[65536], unsigned char *reader);
-void mostrarAnswers(int i, int finalizar, struct DNS_HEADER *dns, struct RES_RECORD answers[20], unsigned char buf[65536], unsigned char *reader);
